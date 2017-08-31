24e2a80e8f1562228a74648531a77d122701b504
Ben M <benmag@users.noreply.github.com>
2017-09-01 01:19:47 +1000

    Implement Namor

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Namor
 Namor - A domain-safe name generator
+
+This package is based on the [Namor package by jsonmaur](https://github.com/jsonmaur/namor).
+
+## Getting started
+**Install via Composer**
+```bash
+composer install benmag/laravel-namor
+```
+
+**Register service provider**
+Add the `NamorServiceProvider` to your `config/app.php` file in the providers key
+```php
+'providers' => [
+    // ... other providers
+    Benmag\Namor\NamorServiceProvider::class,
+]
+```
+
+## Usage
+```
+use Benmag\Namor\Namor;
+
+Namor::generate([number of words], [number of trailing numbers], [seperator character]);
+
+// Defaults to 2 words and 4 trailing numbers
+Namor::generate()
+
+// Generate with 3 words and no numbers
+Namor::generate(3, 0);
+```
+
+## Configuration
+You may also publish the config file to `config/namor.php` which will let you customise the words used to generate the names.
+```bash
+php artisan vendor:publish --provider="Benmag\Namor\NamorServiceProvider"
+```
+
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
--- src/Namor.php
+++ src/Namor.php
@@ -10,5 +10,104 @@ namespace Benmag\Namor;
  */
 class Namor
 {
-    //
+
+    /**
+     * Generate a random name
+     *
+     * @param  int  $words
+     * @param  int  $numbers
+     * @param  string  $char
+     * @return string
+     */
+    public static function generate($words = 2, $numbers = 4, $char = "-")
+    {
+        return static::addTrailingNumber(static::processPattern(static::getPattern($words), $char), $numbers, $char);
+    }
+
+    /**
+     * Returns a language pattern based on the word count of the name.
+     *
+     * @param  int  $words
+     * @return array
+     * @throws \Error
+     */
+    private static function getPattern($words)
+    {
+
+        if($words < 1) {
+            throw new \Error("word count must be above 0");
+        }
+
+        if($words > 4) {
+            throw new \Error("word count cannot be above 4");
+        }
+
+        switch ($words) {
+            default:
+            case 1:
+                $pattern = "noun";
+                break;
+
+            case 2:
+                $pattern = collect(['adjective|noun', 'noun|verb'])->random();
+                break;
+
+            case 3:
+                $pattern = "adjective|noun|verb";
+                break;
+
+            case 4:
+                $pattern = "adjective|noun|noun|verb";
+                break;
+
+        }
+
+        return explode("|", $pattern);
+    }
+
+    /**
+     * Fills a language pattern with actual words from our
+     * dictionary, and turn it into a pipe-cased string.
+     *
+     * @param  array  $pattern
+     * @param  string  $char
+     * @return string
+     */
+    private static function processPattern($pattern, $char)
+    {
+        return implode($char, collect($pattern)->map(function($type) {
+            return collect(config('namor.' . str_plural($type)))->random();
+        })->toArray());
+    }
+
+    /**
+     * Generates and adds a random number to the end of a name.
+     *
+     * @param  string  $name
+     * @param  int  $numbers
+     * @param  string  $char
+     * @return string
+     */
+    private static function addTrailingNumber($name, $numbers, $char)
+    {
+        return $name . ($numbers ? $char . static::randomNumber($numbers) : '');
+    }
+
+    /**
+     * Generate a random number with a fixed length.
+     *
+     * @param $length
+     * @return string
+     */
+    private static function randomNumber($length)
+    {
+        $number = '';
+
+        for($i = 0; $i < $length; $i++) {
+            $number .= mt_rand(0, 9);
+        }
+
+        return $number;
+    }
+
 }
